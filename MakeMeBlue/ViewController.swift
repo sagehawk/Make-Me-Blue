7bb85c80c3de00e612bd39c880fe1cb8443098c7
Sajjad Haq <SageHawk@sages-macbook-air.local>
2016-08-29 11:20:38 -0500

    Initial Commit

---
+++ MakeMeBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeMeBlue
+//
+//  Created by Sage Hawk on 8/29/16.
+//  Copyright © 2016 Sage Hawk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
